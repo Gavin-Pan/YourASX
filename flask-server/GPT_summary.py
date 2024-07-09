74a60f7c2060ee582c1eedab19f47ef6433c4576
Gavin-Pan <gavin.ppan@gmail.com>
2024-07-09 19:52:49 +1000

    hid open ai

--- flask-server/GPT_summary.py
+++ flask-server/GPT_summary.py
@@ -1,5 +1,9 @@
 from openai import OpenAI
 import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
 
 
 alpha_api = "RKE6GIOQI4JM7LYK"
@@ -7,7 +11,7 @@ alpha_api = "RKE6GIOQI4JM7LYK"
 client = OpenAI(
                     organization='org-eXvNlOiMfgKeziZdN6Qd30Ag',
                     project='proj_gAyJmP46wZbkdOLl4bDL3Gvo',
-                    api_key="sk-proj-lyUR5GQT7Q9AfReXlhBUT3BlbkFJGnOwv0BXRtEbzhI00zs4"
+                    api_key=os.environ.get("OPENAI_API_KEY")
                 )
 
 def generate_summary_ONE_article(URL):
@@ -18,7 +22,7 @@ def generate_summary_ONE_article(URL):
                 response_format={ "type": "json_object" },
                 messages=[
                     {"role": "system", "content": "You are a financial analyst, skilled in explaining complex financial concepts to people, with no financial understanding, with clarity and design to output JSON,the JSON format is 'Title', put the key points under 'key points "},
-                    {"role": "user", "content": f"summary this website {URL} into one clear summary to cover everything about this company, less than 200 words and use 5 to 10 dot points. PLEASE USE DOT POINTS"}
+                    {"role": "user", "content": f"summarise this website {URL} into one clear summary to cover everything about this company, less than 200 words. Only words only."}
                 ]
                 )
     result = response.choices[0].message.content
